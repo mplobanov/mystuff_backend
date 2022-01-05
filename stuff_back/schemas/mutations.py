49efff75ea871b64301134c76e299e20ef1971ed
Mikhail Lobanov <mishalob2002@gmail.com>
2022-01-05 14:22:15 +0300

    better

--- stuff_back/schemas/mutations.py
+++ stuff_back/schemas/mutations.py
@@ -1,3 +1,4 @@
+import graphene
 from django.forms import ModelForm
 from graphene_django.forms.mutation import DjangoModelFormMutation
 from graphql_relay import from_global_id
@@ -10,6 +11,7 @@ def custom_mutate(cls, form, info, owner_property, Model):
         raise Exception('Not logged in')
     if 'client_mutation_id' in form.data.keys():
         idd = from_global_id(form.data['client_mutation_id'])[1]
+        print(form.cleaned_data, idd)
         obj = Model.objects.get(pk=idd)
         for k, v in form.cleaned_data.items():
             setattr(obj, k, v)
@@ -68,16 +70,62 @@ class GroupMutation(DjangoModelFormMutation):
         return custom_mutate(cls, form, info, 'gr_owner', Group)
 
 
-class ItemForm(ModelForm):
-    class Meta:
-        model = Item
-        exclude = ['owner']
+class ItemMutationInput(graphene.InputObjectType):
+    id = graphene.String()
+    name = graphene.String(required=True)
+
+    brand = graphene.String(required=True)
+    color = graphene.String(required=True)
+    size = graphene.String(required=True)
+    volume = graphene.String(required=True)
+
+    status = graphene.GlobalID(required=True)
+    location = graphene.GlobalID(required=True)
+    group = graphene.GlobalID(required=True)
 
 
-class ItemMutation(DjangoModelFormMutation):
-    class Meta:
-        form_class = ItemForm
+class ItemMutation(graphene.Mutation):
+    class Arguments:
+        input = ItemMutationInput(required=True)
 
-    @classmethod
-    def perform_mutate(cls, form, info):
-        return custom_mutate(cls, form, info, 'owner', Item)
+    item = graphene.Field(ItemNode)
+
+    @staticmethod
+    def mutate(root, info, input):
+        if not info.context.user.is_authenticated:
+            raise Exception('Not logged in')
+        if Location.objects.get(pk=from_global_id(input['location'])[1]).loc_owner != info.context.user:
+            raise Exception('403 Forbidden Location')
+        if Status.objects.get(pk=from_global_id(input['status'])[1]).st_owner != info.context.user:
+            raise Exception('403 Forbidden Status')
+        if Group.objects.get(pk=from_global_id(input['group'])[1]).gr_owner != info.context.user:
+            raise Exception('403 Forbidden Group')
+        if 'id' in input.keys():
+            item = Item.objects.get(pk=from_global_id(input['id'])[1])
+            if item.owner != info.context.user:
+                raise Exception('403 Forbidden')
+            for key in ['name', 'brand', 'color', 'size', 'volume']:
+                setattr(item, key, input[key])
+            for key in ['location', 'status', 'group']:
+                setattr(item, "{}_id".format(key), from_global_id(input[key])[1])
+            item.save()
+        else:
+            if not input['name']:
+                raise Exception("Name required")
+            item = Item.objects.create(
+                name=input['name'],
+
+                brand=input['brand'],
+                color=input['color'],
+                size=input['size'],
+                volume=input['volume'],
+
+                location_id=from_global_id(input['location'])[1],
+                status_id=from_global_id(input['status'])[1],
+                group_id=from_global_id(input['group'])[1],
+
+                owner_id=info.context.user.id
+            )
+
+        return ItemMutation(item=item)
+
